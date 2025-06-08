c890ce3ac031c214971cb406c47483103630c604
Connor Dougherty-Keehan <connormdk@gmail.com>
2025-06-08 17:40:49 +1000

    update web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3940484786"
+    serviceWorkerVersion: "3068158542"
   }
 });
